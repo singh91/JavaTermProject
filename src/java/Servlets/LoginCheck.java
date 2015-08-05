365a1c03f4c68258aa7644e0cd9ccc84f749f656
c0655613 <c0655613@SB-N208-22.lambton.on.ca>
2015-08-05 13:41:17 -0400

    Worked With Login And Register Pages.

--- src/java/Servlets/LoginCheck.java
+++ src/java/Servlets/LoginCheck.java
@@ -14,8 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
+/*
  * @author Balwant Singh - c0655613
  */
 public class LoginCheck extends HttpServlet {
