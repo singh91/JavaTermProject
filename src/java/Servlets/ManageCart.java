76940ea3bd4acea41bf159a92f42baa9f9343702
c0655613 <c0655613@SB-N208-22.lambton.on.ca>
2015-08-05 13:43:35 -0400

    Managing The Cart And Payment Methods.

--- src/java/Servlets/ManageCart.java
+++ src/java/Servlets/ManageCart.java
@@ -17,8 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- *
+/*
  * @author Balwant Singh - c0655613
  */
 public class ManageCart extends HttpServlet {
